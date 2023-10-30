4bd68d101cb227baddf79f8b025552d034489422
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-10-30 18:58:59 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -2,12 +2,13 @@ class script(object):
     START = """**Hello {} 🤟**
    
 I am **Find Post Bot**.I am best Channel Link Search Bot! 
-I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+I Will filter your channel posts automatically and send it in your group chat when Someone search it.
+🚀Created By @ROCKERSBACKUP """
 
     HELP = """To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @CyniteOfficial.
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @ROCKERSBACKUP.
 - After verification send /connect YourChannelID
 - Example : /connect -100xxxxxxxxxx
 - Done ✅. <b><I>Enjoy 💜❤</I></b> )
@@ -17,10 +18,10 @@ this will help you to remove a indexed channel from your group.
 
 Get connected channels list with - /connections"""
 
-    ABOUT = """Powered By @CyniteBackup
+    ABOUT = """Powered By @ROCKERSBACKUP 
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='tg://user?id=1883570185'>Harman</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='tg://user?id=6605647659'>Jnanesh</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
